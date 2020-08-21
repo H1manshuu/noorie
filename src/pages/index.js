ebeca8b7077a30880363cf204b7dc077584b772c
H1manshuu <chauhanhimanshu151412@gmail.com>
2020-08-21 23:34:44 +0530

    updating prev

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Noor Personal Website"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -17,19 +17,19 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
+          Hey Guys{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
         <p>Welcome to my Personal Website. You are on your home page.</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          This is a personal starter Blog created as an
+           experiment to kill the boredom in lockdown period.
         </p>
-        <p>Now go build something great!</p>
+        <p>Hope You like it</p>
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="35px">Let's Blog</Button>
         </Link>
       </Layout>
     )
